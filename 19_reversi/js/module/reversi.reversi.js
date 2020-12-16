044a2fa4b15afcfd19bd3f18e45ba4ca1aa1fe5c
satoshi ishizaki <e1433104@yahoo.co.jp>
2020-12-17 03:22:12 +0900

    302 board

--- 19_reversi/js/module/reversi.reversi.js
+++ 19_reversi/js/module/reversi.reversi.js
@@ -4,4 +4,18 @@ export class ReversiReversi {
   constructor() {
     this.w = this.h = 8;
   }
+
+  /**
+   * 盤面走査
+   */
+  scnBrd(fnc) {
+    const w = this.w;
+    const h = this.h;
+    const max = w * h;
+    for (let i = 0; i < max; i++) {
+      const x = i % w;
+      const y = (i / w) | 0;
+      fnc(i, x, y);
+    }
+  }
 }
